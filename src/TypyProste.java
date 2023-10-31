1bd3a0d61c2398b139b27c055b51cd57c4388f0e
przem <przemyslawkazimierczak@gmail.com>
2023-10-31 20:24:39 +0100

    Adding gitignore

--- src/TypyProste.java
+++ src/TypyProste.java
@@ -1,2 +1,19 @@
 public class TypyProste {
+    // liczby staloprzecinkowe
+    byte = 127; // 1 bajt od -128 do 127
+    short = 93689; // 2 bajty od -32768 do 32767
+    int = 23290932; // 4 bajty od - 2 147 483 648 do 2 147 483 647
+    long = 2353235423L; // 8 bajtów, bardzo duże liczby, należy deklarować longa literką L na koniec cyfry
+
+    //Liczby zmiennoprzecinkowe
+    float 4,999999f; // max ok 6-7 liczb po przecinku, też deklarujemy literką f na końcu
+    double 3,9999999999999999d; // max ok 15 cyfr po przecinku, też deklarujemy na koniec literką d
+
+    // wartości logiczne
+    boolean prawda = true;
+    boolean fałsz = false;
+
+    // pojedyńczy znak
+    char letter = "a";
+
 }
