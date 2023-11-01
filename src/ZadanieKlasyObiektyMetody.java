5b42fc5fee91312c5eea87155a35166b97ed4702
przem <przemyslawkazimierczak@gmail.com>
2023-11-02 00:57:01 +0100

    Obiekty, klasy i metody

---
+++ src/ZadanieKlasyObiektyMetody.java
@@ -0,0 +1,40 @@
+package src;
+// Stworz 3 obiekty klasy sutdent, przypisz wartości do pol, stworz tabile i dodaj studentów do tablicy
+public class ZadanieKlasyObiektyMetody {
+    public static void main(String[] args) {
+
+        Student Bartek = new Student();
+        Bartek.imie = "Bartek";
+        Bartek.email = "Barte@gmail.com";
+        Bartek.nrIndeksu = 123432;
+        Bartek.nazwisko = "Bobain";
+        Bartek.nick = "Hur";
+
+        Student Mariola = new Student();
+        Mariola.imie = "Mariola";
+        Mariola.email = "Mariola@gmail.com";
+        Mariola.nrIndeksu = 123456;
+        Mariola.nazwisko = "Rzetelna";
+        Mariola.nick = "Dur";
+
+        Student Edward = new Student();
+        Edward.imie = "Edward";
+        Edward.email = "Edward@gmail.com";
+        Edward.nrIndeksu = 123678;
+        Edward.nazwisko = "Śmieszny";
+        Edward.nick = "Łełełe";
+
+               Student[] studenci = new Student[3];
+        studenci[0] = Bartek;
+        studenci[1] = Mariola;
+        studenci[2] = Edward;
+
+        for (int  i=0; i < studenci.length; i++){
+        studenci[i].przedstawSie();
+        studenci[i].zalogujSie();
+        studenci[i].podajNrIndeksu();
+        studenci[i].podajEmail();
+        }
+
+    }
+}
